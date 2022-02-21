9fc45c74ebe7caaf44930227bbc7ad6c7f712963
RicBaller <rickballer007@gmail.com>
2022-02-21 13:21:52 +0100

    Create mc-installer.sh

---
+++ Linux/Ubuntu/Minecraft Server Standalone/mc-installer.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# BAT / CMD goto function
+function goto
+{
+    label=$1
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+# Introduction
+echo "                                       "
+echo "   _____ _                _            "
+echo "  / ____| |              | |           "
+echo " | |    | |__   __ _ _ __| | ___  ___  "
+echo " | |    | |_ \ / _  |  __| |/ _ \/ __| "
+echo " | |____| | | | (_| | |  | |  __/\__ \ "
+echo "  \_____|_| |_|\__,_|_|  |_|\___||___/ "
+echo "                                       "
+echo "                                       "
+
+# Introduction
+echo -e "Charles >> Hello and welcome to the Interworx Installer!"
+sleep 1
+echo -e "Charles >> I'm Charles and I wil be your guide."
+sleep 2
+
+apt update
+
+# Just for the heck of it: how to create a variable where to jump to:
+start=${1:-"start"}
+goto "$start"
+
+: start
+goto_msg="Starting..."
+echo $goto_msg
+# Just jump to the label:
+goto "continue"
+
+: skipped
+goto_msg="This is skipped!"
+echo "$goto_msg"
+
+: continue
+goto_msg="Ended..."
+echo "$goto_msg"
+
+# following doesn't jump to apt update whereas original does
+goto update
