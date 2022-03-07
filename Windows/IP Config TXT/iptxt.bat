28a83395434f1c09e50d90881786e35f14cd2c27
RicBaller <rickballer007@gmail.com>
2022-03-07 10:44:03 +0100

    update

---
+++ Windows/IP Config TXT/iptxt.bat
@@ -0,0 +1,18 @@
+:: Introduction
+@echo off
+echo                                        
+echo    _____ _                _            
+echo   / ____| |              | |           
+echo  | |    | |__   __ _ _ __| | ___  ___  
+echo  | |    | |_ \ / _  |  __| |/ _ \/ __| 
+echo  | |____| | | | (_| | |  | |  __/\__ \ 
+echo   \_____|_| |_|\__,_|_|  |_|\___||___/ 
+echo                                        
+echo                                        
+
+:: Introduction
+echo Charles >> Hello and welcome to the Interworx Installer!
+wait 1
+echo Charles >> I'm Charles and I wil be your guide.
+wait 2
+echo Dit is het
